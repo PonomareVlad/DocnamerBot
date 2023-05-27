1c73c1b5794524568aa1873f1c27186dfb6c5bd2
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-05-27 12:44:58 +0500

    Added Stateless Question

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,11 +1,19 @@
 import {Bot, InputFile} from "grammy";
-import {code, hydrateReply} from "@grammyjs/parse-mode";
+import {StatelessQuestion} from "@grammyjs/stateless-question";
 
 const {TELEGRAM_BOT_TOKEN} = process.env;
 
 export const bot = new Bot(TELEGRAM_BOT_TOKEN);
 
-bot.use(hydrateReply);
+const nameQuestion = new StatelessQuestion("name", async (ctx, file_id) => {
+    const signal = AbortSignal.timeout(9_000);
+    await ctx.replyWithChatAction("upload_document", undefined, signal);
+    const {file_path} = await ctx.api.getFile(file_id, signal);
+    const file = new InputFile(new URL(getFileURL(file_path)), ctx.msg.text);
+    await ctx.replyWithDocument(file, {reply_markup: {remove_keyboard: true}}, signal);
+});
+
+bot.use(nameQuestion.middleware());
 
 const errorHandler = (error, ctx) => {
     console.error(error);
@@ -23,28 +31,27 @@ bot.on(":document", async ctx => {
     try {
         await ctx.replyWithChatAction("typing");
         await ctx.getFile();
-        const {message_id: reply_to_message_id, document: {file_name = "file.txt"} = {}} = ctx.msg;
-        const text = `Select your message and reply to it with new file name, for example:`;
-        await ctx.reply(text, {reply_to_message_id});
-        return ctx.replyFmt(code(file_name), {reply_to_message_id});
+        const {
+            document: {
+                file_id,
+                file_name = "file.txt"
+            } = {}
+        } = ctx.msg;
+        const options = {
+            parse_mode: "Markdown",
+            reply_markup: {
+                force_reply: true,
+                input_field_placeholder: file_name,
+            },
+        };
+        const text = `Send new file name including extension`;
+        return ctx.reply(text + nameQuestion.messageSuffixMarkdown(file_id), options);
     } catch (error) {
         return errorHandler(error, ctx);
     }
 });
 
-bot.on("message", async ctx => {
-    try {
-        const {text, reply_to_message: {document: {file_id} = {}} = {}} = ctx.msg;
-        if (!file_id) return ctx.reply(`Send me any file (as document)`);
-        const signal = AbortSignal.timeout(9_000);
-        await ctx.replyWithChatAction("upload_document", undefined, signal);
-        const {file_path} = await ctx.api.getFile(file_id, signal);
-        const file = new InputFile(new URL(getFileURL(file_path)), text);
-        return ctx.replyWithDocument(file, undefined, signal);
-    } catch (error) {
-        return errorHandler(error, ctx);
-    }
-});
+bot.on("message", async ctx => ctx.reply(`Send me any file (as document)`));
 
 bot.catch(({error, ctx}) => errorHandler(error, ctx));
 
