c0e35fb56cadbee49e243ea911ab1c8abe035447
Vladislav Ponomarev <2877584+PonomareVlad@users.noreply.github.com>
2023-05-27 21:24:28 +0500

    Some fixes

--- src/start.mjs
+++ src/start.mjs
@@ -8,9 +8,9 @@ if (VERCEL_ENV === "development") await bot.start();
 await bot.api.setWebhook(new URL("api/update", `https://${VERCEL_URL}`), {secret_token: Utils.hash(bot.token)});
 
 await Promise.all(Object.entries(Utils.getStringByLocales("bot-description")).map(([locale, string]) => {
-    return bot.api.setMyShortDescription(string, {language_code: Utils.filterLocale(locale)});
+    return bot.api.setMyShortDescription(string, {language_code: Utils.filterLocale(locale)}).catch(console.error);
 }));
 
 await Promise.all(Object.entries(Utils.getStringByLocales("bot-name")).map(([locale, string]) => {
-    return bot.api.setMyName(string, {language_code: Utils.filterLocale(locale)});
+    return bot.api.setMyName(string, {language_code: Utils.filterLocale(locale)}).catch(console.error);
 }));
