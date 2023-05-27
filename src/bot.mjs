02a96908b13c1c79f05e0af24f3dceeb9de4c56c
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-05-27 15:32:50 +0500

    Additional flow

--- src/bot.mjs
+++ src/bot.mjs
@@ -5,54 +5,76 @@ const {TELEGRAM_BOT_TOKEN} = process.env;
 
 export const bot = new Bot(TELEGRAM_BOT_TOKEN);
 
-const nameQuestion = new StatelessQuestion("name", async (ctx, file_id) => {
+const replyToDocumentFilter = ctx => !!ctx?.msg?.reply_to_message?.document?.file_id;
+
+const getFileURL = file_path => `https://api.telegram.org/file/bot${TELEGRAM_BOT_TOKEN}/${file_path}`;
+
+const nameQuestion = new StatelessQuestion("name", async (ctx, additionalState) => {
     const signal = AbortSignal.timeout(9_000);
-    await ctx.replyWithChatAction("upload_document", undefined, signal);
-    const {file_path} = await ctx.api.getFile(file_id, signal);
-    const file = new InputFile(new URL(getFileURL(file_path)), ctx.msg.text);
-    await ctx.replyWithDocument(file, {reply_markup: {remove_keyboard: true}}, signal);
+    const {message_id, chat: {id}} = ctx.msg.reply_to_message;
+    const {file_id, message_id: reply_to_message_id} = JSON.parse(additionalState);
+    const options = {reply_markup: {remove_keyboard: true}, reply_to_message_id};
+    await renameFile(ctx, file_id, ctx.msg.text, options, signal);
+    await Promise.all([
+        ctx.api.deleteMessage(id, message_id, signal),
+        ctx.deleteMessage(signal)
+    ]);
 });
 
-bot.use(nameQuestion.middleware());
-
-const errorHandler = (error, ctx) => {
+const composer = bot.errorBoundary(error => {
+    const {ctx} = error;
     console.error(error);
     if (!ctx?.reply) return;
     const {message_id: reply_to_message_id} = ctx.msg;
     const message = "An error occurred, please try again later";
-    return ctx.reply(error.description || error.message || message, {reply_to_message_id});
-}
-
-const getFileURL = file_path => {
-    return `https://api.telegram.org/file/bot${TELEGRAM_BOT_TOKEN}/${file_path}`;
-}
-
-bot.on(":document", async ctx => {
-    try {
-        await ctx.replyWithChatAction("typing");
-        await ctx.getFile();
-        const {
-            document: {
-                file_id,
-                file_name = "file.txt"
-            } = {}
-        } = ctx.msg;
-        const options = {
-            parse_mode: "Markdown",
-            reply_markup: {
-                force_reply: true,
-                input_field_placeholder: file_name,
-            },
-        };
-        const text = `Send new file name including extension`;
-        return ctx.reply(text + nameQuestion.messageSuffixMarkdown(file_id), options);
-    } catch (error) {
-        return errorHandler(error, ctx);
-    }
+    const text = error.description || error.message || message;
+    return ctx.reply(text, {reply_to_message_id});
 });
 
-bot.on("message", async ctx => ctx.reply(`Send me any file (as document)`));
+composer.use(nameQuestion.middleware());
 
-bot.catch(({error, ctx}) => errorHandler(error, ctx));
+composer.on(":document", async ctx => {
+    await ctx.replyWithChatAction("typing");
+    await ctx.getFile();
+    const {
+        message_id,
+        document: {
+            file_id,
+            file_name = "file.txt"
+        } = {}
+    } = ctx.msg;
+    const options = {
+        parse_mode: "Markdown",
+        reply_markup: {
+            force_reply: true,
+            input_field_placeholder: file_name,
+        },
+    };
+    const text = `Send new file name including extension`;
+    const additionalState = JSON.stringify({file_id, message_id});
+    return ctx.reply(text + nameQuestion.messageSuffixMarkdown(additionalState), options);
+});
+
+composer.on(":text").filter(replyToDocumentFilter, async ctx => {
+    const {
+        document: {
+            file_id
+        } = {},
+        message_id: reply_to_message_id
+    } = ctx.msg.reply_to_message;
+    const options = {reply_to_message_id};
+    const signal = AbortSignal.timeout(9_000);
+    await renameFile(ctx, file_id, ctx.msg.text, options, signal);
+    await ctx.deleteMessage(signal);
+});
+
+composer.on("msg", async ctx => ctx.reply(`Send me any file (as document)`));
+
+async function renameFile(ctx, file_id, filename, options, signal) {
+    void ctx.replyWithChatAction("upload_document", undefined, signal);
+    const {file_path} = await ctx.api.getFile(file_id, signal);
+    const file = new InputFile(new URL(getFileURL(file_path)), filename);
+    await ctx.replyWithDocument(file, options, signal);
+}
 
 export default bot;
